632d8d6193ad0a5940da915e7973d2a0e3ef4b22
Gol-Stan <loww.101011@gmail.com>
2025-09-16 21:25:29 +0300

    About page done

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Works.css';
 import { Card } from 'react-bootstrap';
 import ArtCourseImg1 from '../../utils/images/int3.jpg';
-import ArtCourseImg2 from '../../utils/images/int2.jpg';
+import ArtCourseImg2 from '../../utils/images/int3.jpg';
 import ArtCourseImg3 from '../../utils/images/int1.jpg';
 import ArtCourseImg4 from '../../utils/images/home-1622401_1920.jpg';
 import FaqAccordion from "../../components/FaqAccordion/FaqAccordion";
@@ -10,25 +10,25 @@ import FaqAccordion from "../../components/FaqAccordion/FaqAccordion";
 const works = [
     {
         id: 1,
-        image: [ArtCourseImg1],
+        img: [ArtCourseImg1],
         title: 'Art Course One',
         description: 'Lorem Lorem Lorem Lorem'
     },
     {
         id: 2,
-        image: [ArtCourseImg2],
+        img: [ArtCourseImg2],
         title: 'Art Course Two',
         description: 'Lpus Lupus Lupus'
     },
     {
         id: 3,
-        image: [ArtCourseImg3],
+        img: [ArtCourseImg3],
         title: 'Art Course Three',
         description: 'Dolce Dolce Dolce Dolce'
     },
     {
         id: 4,
-        image: [ArtCourseImg4],
+        img: [ArtCourseImg4],
         title: 'Art Course Four',
         description: 'Pie Pie Pie Pie'
     },
@@ -44,12 +44,12 @@ function Works() {
                 </div>
             </header>
 
-            <div className='conteiner py-5'>
+            <div className='container py-5'>
                 <div className='row g-4'>
                     {works.map((work) => (
                         <div key={work.id} className='col-lg-6'>
                             <Card className='text-white shadow scale-hover-effect'>
-                                <Card.Img src={work.image} />
+                                <Card.Img src={work.img} className='img-fluid' width='500' height='400' />
                                 <Card.ImgOverlay className='d-flex flex-column align-items-center justify-content-center p-md-5'>
                                     <Card.Title className='text-center fs-1 text-danger'>{work.title}</Card.Title>
                                     <Card.Text className='text-center'>{work.description}</Card.Text>
@@ -61,7 +61,7 @@ function Works() {
                 </div>
             </div>
 
-            <div className='bgdark text-light py-5'>
+            <div className='bgdark text-dark py-5'>
                 <FaqAccordion />
             </div>
 

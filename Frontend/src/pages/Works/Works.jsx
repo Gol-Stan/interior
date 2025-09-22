aba10f23fc4750acc3aff9ad22efb78dab26137e
Gol-Stan <loww.101011@gmail.com>
2025-09-22 22:23:45 +0300

    language works added

--- Frontend/src/pages/Works/Works.jsx
+++ Frontend/src/pages/Works/Works.jsx
@@ -6,50 +6,34 @@ import ArtCourseImg2 from '../../utils/images/int2.jpg';
 import ArtCourseImg3 from '../../utils/images/int1.jpg';
 import ArtCourseImg4 from '../../utils/images/home-1622401_1920.jpg';
 import FaqAccordion from "../../components/FaqAccordion/FaqAccordion";
+import { useTranslation } from "react-i18next";
 
-const works = [
-    {
-        id: 1,
-        img: [ArtCourseImg1],
-        title: 'Art Course One',
-        description: 'Lorem Lorem Lorem Lorem'
-    },
-    {
-        id: 2,
-        img: [ArtCourseImg2],
-        title: 'Art Course Two',
-        description: 'Lpus Lupus Lupus'
-    },
-    {
-        id: 3,
-        img: [ArtCourseImg3],
-        title: 'Art Course Three',
-        description: 'Dolce Dolce Dolce Dolce'
-    },
-    {
-        id: 4,
-        img: [ArtCourseImg4],
-        title: 'Art Course Four',
-        description: 'Pie Pie Pie Pie'
-    },
-]
+const images = [ArtCourseImg1, ArtCourseImg2, ArtCourseImg3, ArtCourseImg4];
 
 function Works() {
+    const { t } = useTranslation();
+    const works = t("works.list", { returnObjects: true });
+
     return (
         <div className='works-page'>
             <header className='height-75'>
                 <div className='container h-100 d-flex flex-column align-items-center justify-content-center text-light'>
-                    <h1 className='text-center fw-semibold'>Our Works</h1>
-                    <p className='text-center w-75 mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!</p>
+                    <h1 className='text-center fw-semibold'>{t("works.title")}</h1>
+                    <p className='text-center w-75 mb-5'>{t("works.subtitle")}</p>
                 </div>
             </header>
 
             <div className='container py-5'>
                 <div className='row g-4'>
-                    {works.map((work) => (
+                    {works.map((work, index) => (
                         <div key={work.id} className='col-lg-6'>
                             <Card className='text-white shadow scale-hover-effect'>
-                                <Card.Img src={work.img} className='img-fluid' width='500' height='400' />
+                                <Card.Img
+                                    src={images[index]}
+                                    className='img-fluid'
+                                    width='500'
+                                    height='400'
+                                />
                                 <Card.ImgOverlay className='d-flex flex-column align-items-center justify-content-center p-md-5'>
                                     <Card.Title className='text-center fs-1 text-danger'>{work.title}</Card.Title>
                                     <Card.Text className='text-center'>{work.description}</Card.Text>

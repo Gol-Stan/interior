b00de4c8e77470d633078c04c98d1e3ee4d75fdd
Gol-Stan <loww.101011@gmail.com>
2025-09-23 22:22:16 +0300

    language about added

--- Frontend/src/pages/About/About.jsx
+++ Frontend/src/pages/About/About.jsx
@@ -6,41 +6,30 @@ import ChooseSection from "../../components/ChooseSection/ChooseSection";
 import Client1 from '../../utils/images/115783931601b5c.webp'
 import Client2 from '../../utils/images/company-logo-transparent-png-19.png'
 import Client3 from '../../utils/images/_-mWIBoT95TO3mX.png'
+import { useTranslation } from 'react-i18next';
 
-const clients = [
-    {
-        id: 1,
-        img: [Client1]
-    },
-    {
-        id: 2,
-        img: [Client2]
-    },
-    {
-        id: 3,
-        img: [Client3]
-    },
-
-];
+const images = [Client1, Client2, Client3];
 
 function About() {
+    const { t } = useTranslation();
+
     return (
         <div className='about-page'>
             <header className='height-75'>
                 <div className='container h-100 d-flex flex-column align-items-center justify-content-center text-light'>
-                    <h1 className='text-center fw-semibold'>About Us</h1>
-                    <p className='text-center w-75 mb-5'>Lorem Morem Borem</p>
+                    <h1 className='text-center fw-semibold'>{t("about.title")}</h1>
+                    <p className='text-center w-75 mb-5'>{t("about.subtitle")}</p>
                 </div>
             </header>
 
             <div className='container my-5'>
                 <div className='row'>
                     <div className='col-lg-6 d-flex flex-column justify-content-center mb-4 mb-lg-0'>
-                        <h2 className='mb-4 mb-lg-5'>Study with Us</h2>
-                        <p>Lorem Orem Dorem Korem</p>
-                        <p className='mb-4 mb-lg-5'>Lorem Orem Dorem Korem</p>
+                        <h2 className='mb-4 mb-lg-5'>{t("about.study_title")}</h2>
+                        <p>{t("about.study_p1")}</p>
+                        <p className='mb-4 mb-lg-5'>{t("about.study_p2")}</p>
                         <Link to='/contact'>
-                            <button type='button' className='btn btn-danger btn-lg mx-o mx-sm my-2 my-sm-0'>Contact Us</button>
+                            <button type='button' className='btn btn-danger btn-lg mx-o mx-sm my-2 my-sm-0'>{t("about.contact_btn")}</button>
                         </Link>
                     </div>
                     <div className='col-lg-6 d-flex justify-content-center'>
@@ -55,11 +44,11 @@ function About() {
 
             <div className='bg-body-tertiary py-5'>
                 <div className='container'>
-                    <h2 className='text-center mb-5'>Our clients</h2>
+                    <h2 className='text-center mb-5'>{t("about.clients_title")}</h2>
                     <div className='row g-4'>
-                        {clients.map((client) => (
-                            <div key={client.id} className='col-md-4'>
-                                <img src={client.img} className='img-fluid' alt='' />
+                        {images.map((client, index) => (
+                            <div key={index} className='col-md-4'>
+                                <img src={images[index]} className='img-fluid' alt='' />
                             </div>
                             ))}
                     </div>
